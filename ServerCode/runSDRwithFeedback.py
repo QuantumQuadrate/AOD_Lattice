037224b38854747030706222d5fe16c51a41c4d3
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2019-06-18 10:18:55 -0500

    fixing waveManager

--- ServerCode/runSDRwithFeedback.py
+++ ServerCode/runSDRwithFeedback.py
@@ -6,6 +6,6 @@ from Tools.TrapFeedback import TrapFeedback
 waveManager = WaveformManager("Resources/waveformArguments.json")
 trapFeedback = TrapFeedback(waveManager, 0, 14353509, "128.104.162.32")
 
-SDR = SoftwareDefinedRadio(waveMonitor)
+SDR = SoftwareDefinedRadio(waveManager)
 SDR.initializeSDR()
 SDR.startMonitor()
