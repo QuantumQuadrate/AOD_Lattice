11a09db33c0902046109bebfa8b3219f559c190b
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2019-06-28 16:20:29 -0500

    starting camera capture

--- cameraServer.py
+++ cameraServer.py
@@ -5,6 +5,7 @@ from Tools.peakAnalysisTools import peakAnalysisTools
 cameraSerial = 14353509
 camera = BlackflyCamera({'serial': cameraSerial})
 camera.initialize()
+camera.start_capture()
 peakTool = peakAnalysisTools(camera)
 Server = CameraServer()
 Server.runServer(peakTool)
