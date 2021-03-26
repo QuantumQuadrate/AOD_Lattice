779135f60f884b5e92cec0ebb612127485e4d94b
TrentGraham <tmgraham3@wisc.edu>
2021-03-26 15:58:59 -0500

    Added CameraManager for the new SDK

---
+++ Tools/CameraManager.py
@@ -0,0 +1,210 @@
+
+import PySpin
+
+class CameraManager():
+    def __init__(self, serial):
+        """
+        Initializes camera manager with target camera. Camera will not be initialized if no camera is found.
+        :param serial: Serial number of target camera
+        """
+        self.system = PySpin.System.GetInstance()
+        self.cam_list = self.system.GetCameras()
+
+        self.cam = self.get_camera(serial)
+        if self.cam is not None:
+            self.cam.Init()
+
+
+    def get_camera(self, serial):
+        """
+        Gets camera by searching through available cameras and comparing serial number.
+        :param serial: Serial number of target camera
+        :return: Camera if found else none
+        """
+        for cam in self.cam_list:
+            nodemap_tldevice = cam.GetTLDeviceNodeMap()
+            node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+            if PySpin.IsAvailable(node_device_serial_number) and PySpin.IsReadable(node_device_serial_number):
+                device_serial_number = node_device_serial_number.GetValue()
+                if str(serial) == device_serial_number:
+                    print("Found camera %s" % serial)
+                    return cam
+
+        return None
+
+    def get_image(self):
+        """
+        This function was imported from the Acquisition.py example from the SDK.
+
+        :param cam: Camera to acquire images from.
+        :param nodemap: Device nodemap.
+        :param nodemap_tldevice: Transport layer device nodemap.
+        :type cam: CameraPtr
+        :type nodemap: INodeMap
+        :type nodemap_tldevice: INodeMap
+        :return: True if successful, False otherwise.
+        :rtype: bool
+        """
+
+        nodemap_tldevice = self.cam.GetTLDeviceNodeMap()
+        nodemap = self.cam.GetNodeMap()
+        print('*** IMAGE ACQUISITION ***\n')
+        try:
+            result = True
+
+            # Set acquisition mode to continuous
+            #
+            #  *** NOTES ***
+            #  Because the example acquires and saves 10 images, setting acquisition
+            #  mode to continuous lets the example finish. If set to single frame
+            #  or multiframe (at a lower number of images), the example would just
+            #  hang. This would happen because the example has been written to
+            #  acquire 10 images while the camera would have been programmed to
+            #  retrieve less than that.
+            #
+            #  Setting the value of an enumeration node is slightly more complicated
+            #  than other node types. Two nodes must be retrieved: first, the
+            #  enumeration node is retrieved from the nodemap; and second, the entry
+            #  node is retrieved from the enumeration node. The integer value of the
+            #  entry node is then set as the new value of the enumeration node.
+            #
+            #  Notice that both the enumeration and the entry nodes are checked for
+            #  availability and readability/writability. Enumeration nodes are
+            #  generally readable and writable whereas their entry nodes are only
+            #  ever readable.
+            #
+            #  Retrieve enumeration node from nodemap
+
+            # In order to access the node entries, they have to be casted to a pointer type (CEnumerationPtr here)
+            node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+            if not PySpin.IsAvailable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
+                print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
+                return False
+
+            # Retrieve entry node from enumeration node
+            node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
+            if not PySpin.IsAvailable(node_acquisition_mode_continuous) or not PySpin.IsReadable(node_acquisition_mode_continuous):
+                print('Unable to set acquisition mode to continuous (entry retrieval). Aborting...')
+                return False
+
+            # Retrieve integer value from entry node
+            acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+
+            # Set integer value from entry node as new value of enumeration node
+            node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+
+            print('Acquisition mode set to continuous...')
+
+            #  Begin acquiring images
+            #
+            #  *** NOTES ***
+            #  What happens when the camera begins acquiring images depends on the
+            #  acquisition mode. Single frame captures only a single image, multi
+            #  frame catures a set number of images, and continuous captures a
+            #  continuous stream of images. Because the example calls for the
+            #  retrieval of 10 images, continuous mode has been set.
+            #
+            #  *** LATER ***
+            #  Image acquisition must be ended when no more images are needed.
+            self.cam.BeginAcquisition()
+
+            print('Acquiring images...')
+
+            #  Retrieve device serial number for filename
+            #
+            #  *** NOTES ***
+            #  The device serial number is retrieved in order to keep cameras from
+            #  overwriting one another. Grabbing image IDs could also accomplish
+            #  this.
+            device_serial_number = ''
+            node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+            if PySpin.IsAvailable(node_device_serial_number) and PySpin.IsReadable(node_device_serial_number):
+                device_serial_number = node_device_serial_number.GetValue()
+                print('Device serial number retrieved as %s...' % device_serial_number)
+
+            # Retrieve, convert, and save images
+            for i in range(1):
+                try:
+
+                    #  Retrieve next received image
+                    #
+                    #  *** NOTES ***
+                    #  Capturing an image houses images on the camera buffer. Trying
+                    #  to capture an image that does not exist will hang the camera.
+                    #
+                    #  *** LATER ***
+                    #  Once an image from the buffer is saved and/or no longer
+                    #  needed, the image must be released in order to keep the
+                    #  buffer from filling up.
+                    image_result = self.cam.GetNextImage()
+
+                    #  Ensure image completion
+                    #
+                    #  *** NOTES ***
+                    #  Images can easily be checked for completion. This should be
+                    #  done whenever a complete image is expected or required.
+                    #  Further, check image status for a little more insight into
+                    #  why an image is incomplete.
+                    if image_result.IsIncomplete():
+                        print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+
+                    else:
+
+                        #  Print image information; height and width recorded in pixels
+                        #
+                        #  *** NOTES ***
+                        #  Images have quite a bit of available metadata including
+                        #  things such as CRC, image status, and offset values, to
+                        #  name a few.
+                        width = image_result.GetWidth()
+                        height = image_result.GetHeight()
+                        print('Grabbed Image %d, width = %d, height = %d' % (i, width, height))
+
+                        #  Convert image to mono 8
+                        #
+                        #  *** NOTES ***
+                        #  Images can be converted between pixel formats by using
+                        #  the appropriate enumeration value. Unlike the original
+                        #  image, the converted one does not need to be released as
+                        #  it does not affect the camera buffer.
+                        #
+                        #  When converting images, color processing algorithm is an
+                        #  optional parameter.
+                        #image_converted = image_result.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR)
+                        image_data = image_result.GetNDArray()
+
+                        #  Release image
+                        #
+                        #  *** NOTES ***
+                        #  Images retrieved directly from the camera (i.e. non-converted
+                        #  images) need to be released in order to keep from filling the
+                        #  buffer.
+                        image_result.Release()
+                        print('')
+
+                except PySpin.SpinnakerException as ex:
+                    print('Error: %s' % ex)
+                    return False
+
+            #  End acquisition
+            #
+            #  *** NOTES ***
+            #  Ending acquisition appropriately helps ensure that devices clean up
+            #  properly and do not need to be power-cycled to maintain integrity.
+            self.cam.EndAcquisition()
+
+        except PySpin.SpinnakerException as ex:
+            print('Error: %s' % ex)
+            return False
+
+        return image_data
+
+    def __del__(self):
+        """
+        This method ensures clean up of the spinpy variables.
+        """
+        if self.cam is not None:
+            self.cam.DeInit()
+            del self.cam
+        self.cam_list.Clear()
+        self.system.ReleaseInstance()
--- cameraServer.py
+++ cameraServer.py
@@ -1,11 +1,13 @@
-from Tools.BlackflyCamera import BlackflyCamera
 from Tools.Servers import CameraServer
+from Tools.CameraManager import CameraManager
 from Tools.peakAnalysisTools import peakAnalysisTools
 
-cameraSerial = 14353509
-camera = BlackflyCamera({'serial': cameraSerial})
-camera.initialize()
-camera.start_capture()
-peakTool = peakAnalysisTools(camera)
-Server = CameraServer()
-Server.runServer(peakTool)
+cameraSerial = 19287346
+camera = CameraManager(cameraSerial)
+
+if camera.cam is None:
+    print("Could not find camera  ... not starting server")
+else:
+    peakTool = peakAnalysisTools(camera)
+    Server = CameraServer()
+    Server.runServer(peakTool)
